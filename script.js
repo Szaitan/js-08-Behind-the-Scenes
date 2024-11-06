8d65bbb16731b44a040553efebec423c0acc8168
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2024-11-06 19:21:46 +0100

    Better understanding of scopes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age} old.🎈 Born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you are Millenial, ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Michał';
+calcAge(1990);
